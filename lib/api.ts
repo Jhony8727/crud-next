2e558e6826cb636ceb1d62fb01f74e93ff16cc50
Eduardo Fernandes <dudufernandes021@gmail.com>
2026-06-01 21:38:16 -0300

    configuraçao axios

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configure a URL base do seu backend aqui
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
